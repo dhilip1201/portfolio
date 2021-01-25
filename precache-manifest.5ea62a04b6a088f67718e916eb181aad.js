bab908711a8b27c0f98cba25745651f3013e5130
dhilip1201 <dhilipbeece006@gmail.com>
2021-01-25 08:03:08 +0530

    Updates

---
+++ precache-manifest.5ea62a04b6a088f67718e916eb181aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16a31fe536d1db8af8d350ba8ba696d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f08795b3da6a1aa631",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f08795b3da6a1aa631",
+    "url": "/static/js/main.df54b08a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
